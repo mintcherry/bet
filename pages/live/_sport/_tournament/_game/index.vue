d8d0751be83eebe57574fc133aa0825c1ba43ded
charrywork@yandex.ru <PJA9Xy3yR>
2022-06-08 00:58:45 +0300

    fix

--- pages/live/_sport/_tournament/_game/index.vue
+++ pages/live/_sport/_tournament/_game/index.vue
@@ -1,14 +1,74 @@
 <template>
-  <div class="game-page"></div>
+  <div v-if="gameIsAvailable" class="game-page">
+    <Information
+      :opponents="opponents"
+      :date="date"
+      :tournament="tournament"
+    />
+    
+  </div>
+  <div v-else class="game-page__empty">
+    Событие не доступно
+  </div>
 </template>
 
 <script>
+import Information from '@/components/Game/Information';
+
+import { API_MODE, API_TOKEN } from '@/utils/apiParams';
+
 export default {
   name: 'GamePage',
-  layout: 'EventLayout',
+  layout: 'GameLayout',
+  components: {
+    Information,
+  },
+  
+  async asyncData({ $axios, route }) {
+    let params = {
+      id: route.query.gameId,
+      mode: null,
+    }
+    
+    route.name.includes(API_MODE.LIVE)
+        ? params.mode = API_MODE.LIVE
+        : params.mode = API_MODE.LINE
+    
+    try {
+      const response = await $axios.get(`/event/${params.id}/sub/${params.mode}/ru`, {
+        headers: {
+          'Package': API_TOKEN,
+        },
+      });
+      
+      return {
+        event: response.data.body,
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  computed: {
+    gameIsAvailable() {
+      return this.event !== undefined;
+    },
+    opponents() {
+      return this.event.opp_1_name + ' - ' + this.event.opp_2_name;
+    },
+    date() {
+      return this.event.game_start;
+    },
+    tournament() {
+      return this.event.tournament_name;
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-
+.game-page__empty {
+  @include flexCenter;
+  @include font(24px, 30px, 600, $black);
+  margin-top: 50px;
+}
 </style>
