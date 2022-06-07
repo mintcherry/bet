6c330bc8f7a4239e60ef57f5bc7121e2600eeba3
charrywork@yandex.ru <PJA9Xy3yR>
2022-06-07 23:57:51 +0300

    feat(game page): Получение инфы отдельной игры.

--- pages/line/_sport/_tournament/_game/index.vue
+++ pages/line/_sport/_tournament/_game/index.vue
@@ -1,14 +1,55 @@
 <template>
-  <div class="game-page"></div>
+  <div v-if="gameIsAvailable" class="game-page">
+  
+  </div>
+  <div v-else class="game-page__empty">
+    Событие не доступно
+  </div>
 </template>
 
 <script>
+import { API_MODE, API_TOKEN } from '@/utils/apiParams';
+
 export default {
   name: 'GamePage',
   layout: 'EventLayout',
+  
+  async asyncData({ $axios, route }) {
+    let params = {
+      id: route.query.gameId,
+      mode: null,
+    }
+    
+    route.name.includes(API_MODE.LIVE)
+        ? params.mode = API_MODE.LIVE
+        : params.mode = API_MODE.LINE
+    
+    try {
+      const response = await $axios.get(`/event/${params.id}/sub/${params.mode}/ru`, {
+        headers: {
+          'Package': API_TOKEN,
+        },
+      });
+      
+      return {
+        event: response.data.body,
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  computed: {
+    gameIsAvailable() {
+      return this.event !== undefined;
+    },
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.game-page__empty {
+  @include flexCenter;
+  @include font(24px, 30px, 600, $black);
+  margin-top: 50px;
+}
 </style>
