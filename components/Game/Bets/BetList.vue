3c144b6bc975c5793786f215a7bab17a42b51d1d
charrywork@yandex.ru <PJA9Xy3yR>
2022-06-08 23:13:22 +0300

    feat(game coupon): Страница вывода всех купонов для события.

---
+++ components/Game/Bets/BetList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bet-list">
+    <div class="bet-list__title">
+      {{ betList.group_name }}
+    </div>
+    
+    <div class="bet-list__wrapper">
+      <BetListItem
+          v-for="(coupon, i) of availableCoupon"
+          :key="i"
+          :coupon="coupon"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import BetListItem from '@/components/Game/Bets/BetListItem';
+
+export default {
+  name: 'BetList',
+  components: {
+    BetListItem,
+  },
+  
+  props: {
+    betList: Object,
+  },
+  computed: {
+    availableCoupon() {
+      return this.betList.oc_list;
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bet-list {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+  break-inside: avoid-column;
+}
+
+.bet-list__title {
+  @include font(16px, 20px, 600, $white);
+  padding: 10px;
+  background: $bard;
+}
+
+.bet-list__wrapper {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ components/Game/Bets/BetListItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="bet-list-item">
+    <div class="bet-list-item__name">
+      {{ coupon.oc_name }}
+    </div>
+    <div class="bet-list-item__value">
+      {{ coupon.oc_rate }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BetListItem',
+  
+  props: {
+    coupon: Object,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.bet-list-item {
+  display: flex;
+  height: 100%;
+  background: $white;
+  cursor: pointer;
+  
+  &:not(:last-child) {
+    border-bottom: 1px solid #F5F5F5;
+  }
+  
+  &:hover {
+    .bet-list-item__value {
+      background: $black;
+      color: $white;
+      opacity: .9;
+    }
+  }
+}
+
+.bet-list-item__name {
+  @include font(14px, 18px, 600, $black);
+  width: 90%;
+  height: 100%;
+  padding: 7px 5px 7px 10px;
+}
+
+.bet-list-item__value {
+  @include flexCenter;
+  @include font(14px, 18px, 600, $black);
+  width: 10%;
+  padding: 7px;
+  background: #ECECEC;
+  box-sizing: border-box;
+  transition: 250ms;
+}
+</style>
